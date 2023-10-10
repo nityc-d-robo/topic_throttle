4b27a795c31577dfe78a29e8270b649cc4712480
sarukiti <sarukiti1891@gmail.com>
2023-10-11 01:40:31 +0900

    完璧なthrottleが完成

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,39 @@
 use safe_drive::{
-    context::Context, error::DynError, msg::common_interfaces::geometry_msgs,
+    context::Context, error::DynError, msg::common_interfaces::geometry_msgs, topic::subscriber::Subscriber
 };
-use std::{rc::Rc, cell::RefCell, time::Duration};
+use std::{sync::{Arc, Mutex}, time::Duration};
 
-fn main() -> Result<(), DynError>{
+#[async_std::main]
+async fn main() -> Result<(), DynError>{
     let ctx = Context::new()?;
     let node = ctx.create_node("topic_throttle", None, Default::default())?;
     let subscriber = node.create_subscriber::<geometry_msgs::msg::Twist>("cmd_vel", None)?;
     let publisher = node.create_publisher::<geometry_msgs::msg::Twist>("cmd_vel/robocon_2023", None)?;
-    let mut selector = ctx.create_selector()?;
 
-    let msg = Rc::new(RefCell::new(geometry_msgs::msg::Twist::new().unwrap()));
-    let sub_msg = msg.clone();
+    let pub_msg = Arc::new(Mutex::new(geometry_msgs::msg::Twist::new().unwrap()));
+    let sub_msg = pub_msg.clone();
 
-    selector.add_wall_timer(
-        "publisher",
-        Duration::from_millis(100),
-        Box::new(move || {
-            publisher.send(&msg.borrow()).unwrap();
-        }),
-    );
 
-    selector.add_subscriber(
-        subscriber,
-        Box::new(move |_msg| {
-            sub_msg.borrow_mut().linear.x = _msg.linear.x;
-            sub_msg.borrow_mut().linear.y = _msg.linear.y;
-            sub_msg.borrow_mut().angular.z = _msg.angular.z;
-        }),
-    );
 
-    loop{
-        selector.wait()?;
+    let task_pub = async_std::task::spawn(async move {
+        loop{
+            publisher.send(&pub_msg.lock().unwrap()).unwrap();
+            async_std::task::sleep(Duration::from_millis(100)).await;
+        }
+    });
+    let task_sub = async_std::task::spawn(receiver(subscriber, sub_msg));
+
+    task_pub.await;
+    task_sub.await?;
+
+    Ok(())
+}
+
+async fn receiver(mut subscriber: Subscriber<geometry_msgs::msg::Twist>, sub_msg: Arc<Mutex<geometry_msgs::msg::Twist>>) -> Result<(), DynError> {
+    loop {
+        let _msg = subscriber.recv().await?;
+        sub_msg.lock().unwrap().linear.x = _msg.linear.x;
+        sub_msg.lock().unwrap().linear.y = _msg.linear.y;
+        sub_msg.lock().unwrap().angular.z = _msg.angular.z;
     }
 }
